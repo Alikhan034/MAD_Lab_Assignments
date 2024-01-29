225d1d696bf44c7dbabfc499cf2a7b278a22938e
Muhammad Ali Khan <102279247+Alikhan034@users.noreply.github.com>
2024-01-29 17:18:40 +0500

    Update Lab_A1.js

--- Lab_A1.js
+++ Lab_A1.js
@@ -1,7 +1,5 @@
-/*Name: Muhammad Ali Khan
-Reg#: SP20-BCS-034 
-MADL Assignment $1*/
-/*Problem 1:
+
+/*
 1.	Write a function called sandwich calculator. This should accept one value: bread
 2.	The function should return the total number of possible sandwiches based on the amount of breads available. I need 2 breads to make one sandwich, so if there are 10 breads, it should return 5. Test your function with console.log.
 3.	Extend your function so it accepts two values, bread and cheese.
